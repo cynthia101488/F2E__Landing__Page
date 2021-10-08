c1e12af7d4bf16829622728487122d828ca694c3
cynthia101488 <76106817+cynthia101488@users.noreply.github.com>
2021-10-08 14:54:01 +0800

    20211008 update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
+const elemPic = document.querySelector('#Banner');
 const elemNav = document.querySelector('#Nav');
 const elemNavLink = document.querySelectorAll('.nav__item a');
-const elemPic = document.querySelector('#Banner');
-const elemPlayBtn = document.querySelector('#PlayBtn');
 const elemVideo = document.querySelector('#Video');
 const picData = ['banr_ice.jpg', 'banr_mountain.jpg', 'banr_run.jpg', 'banr_snow.jpg'];
-const elemArr = ['ani-fade-in-top', 'ani-fade-in-down']
+const arrAni = ['ani-fade-in-top', 'ani-fade-in-down'];
 let picCount = 0;
 let endTime = '';
 let personNum = 0;
 let isEnd = false;
-let timer = setInterval(setTime, 1000);
+let isNavRender = false;
+let timer = setInterval(renderTime, 1000);
 
 setInterval(renderPic, 5000);
 getData();
@@ -17,7 +17,7 @@ setEvent();
 
 function renderPic() {
   picCount = swap(picCount);
-  elemPic.style = `background-image: url('./images/${picData[picCount]}')`;
+  elemPic.style = `background-image: url('../images/${picData[picCount]}')`;
 }
 
 function swap(picCount) {
@@ -25,7 +25,7 @@ function swap(picCount) {
 }
 
 function getData() {
-  const api = './data/activity.json';
+  const api = '../data/activity.json';
   fetch(api)
     .then(res => res.json())
     .then(data => {
@@ -36,41 +36,24 @@ function getData() {
 function setTemplate(data) {
   endTime = new Date(data['endTime']);
   personNum = data['personNum'];
-  setTime();
-
-  const elemPersonNum = document.querySelector('#personNum');
-  let numstr = '';
-  if (personNum < 100) {
-    numstr = `<div class="signup__info" id="personNum">
-                <span class="signup__num">已有</span>
-                <span class="signup__num signup__num--lg">${personNum}</span>
-                <span class="signup__num">人報名</span>
-              </div>
-              <div class="signup__btn">
-                <button class="btn">搶先報名 »</button>
-              </div>`;
-  } else {
-    if (!isEnd) {
-      numstr = `<span class="signup__full">已爆滿!</span>`
-    } else {
-      numstr = `<span class="signup__full">已額滿!</span>`
-    }
-  }
-  elemPersonNum.innerHTML = numstr;
+  let arrList = data['list'];
+  renderTime();
+  renderProgress(arrList);
+  renderPerson();
 }
 
-function setTime() {
-  const elemCountDown = document.querySelector('#CountDown');
-  const currentTime = new Date();
-  const diff = endTime - currentTime;
-  let timestr = '';
+function renderTime() {
+  const elemTime = document.querySelector('#Time');
+  const now = new Date();
+  const diff = endTime - now;
+  let str = '';
   if (diff > 0) {
     if (personNum < 100) {
       const day = Math.floor(diff / 1000 / 60 / 60 / 24);
       const hour = Math.floor(diff / 1000 / 60 / 60) % 24;
       const min = Math.floor(diff / 1000 / 60) % 60;
       const sec = Math.floor(diff / 1000) % 60;
-      timestr = `<p class="signup__sale">優惠倒數</p>
+      str = `<p class="signup__sale">優惠倒數</p>
                 <div class="signup__countdown">
                   <span class="signup__time signup__time--lg">${day}</span>
                   <span class="signup__time">天</span>
@@ -82,55 +65,112 @@ function setTime() {
                   <span class="signup__time">秒</span>
                 </div>`;
     } else {
-      timestr = `<p class="signup__sale">贈送完畢</p>
+      str = `<p class="signup__sale">贈送完畢</p>
                 <span class="signup__time">我們提早結束優惠</span>`;
       clearInterval(timer);
     }
   } else {
-    timestr = `<p class="signup__sale">優惠活動結束</p>
+    str = `<p class="signup__sale">優惠活動結束</p>
               <span class="signup__time">請再關注我們的優惠時間</span>`;
     isEnd = true;
     clearInterval(timer);
   }
-  elemCountDown.innerHTML = timestr;
+  elemTime.innerHTML = str;
+}
+
+function renderProgress(arr) {
+  const elemProgress = document.querySelector('#Progress');
+  const elemBar = document.querySelector('#Bar');
+  let str = '';
+  arr.forEach((item, index) => {
+    str += `<li class="signup__item item-${index + 1}">
+                    <div class="signup__top">
+                      <span class="signup__text text--sm">達</span>
+                      <span class="signup__text text--sm">${item.level}</span>
+                      <span class="signup__text text--sm">人</span>
+                    </div>
+                    <div class="signup__box ${personNum >= item.level ? 'signup__box--complete' : ''}"></div>
+                    <div class="signup__bottom">
+                      <span class="signup__text text--sm">送</span>
+                      <span class="signup__text text--sm">${item.productName}</span>
+                    </div>
+                  </li>`
+  });
+
+  elemProgress.innerHTML = `<li class="signup__item item-0">
+                              <div class="signup__box signup__box--complete"></div>
+                              <div class="signup__bottom">
+                                <span class="signup__text text--sm">預購開始</span>
+                              </div>
+                            </li>` + str;
+
+  elemBar.style = `width: ${personNum + '%'}`;
+}
+
+function renderPerson() {
+  const elemPersonNum = document.querySelector('#PersonNum');
+  let str = '';
+  if (personNum < 100) {
+    str = `<div class="signup__info" id="personNum">
+                <span class="signup__text">已有</span>
+                <span class="signup__text text--lg">${personNum}</span>
+                <span class="signup__text">人報名</span>
+              </div>
+              <div class="signup__btn">
+                <button class="btn">搶先報名 »</button>
+              </div>`;
+  } else {
+    if (!isEnd) {
+      str = `<span class="signup__full">已爆滿!</span>`
+    } else {
+      str = `<span class="signup__full">已額滿!</span>`
+    }
+  }
+  elemPersonNum.innerHTML = str;
 }
 
 function setEvent() {
-  let isNavRender = false;
-  document.addEventListener('scroll', ()=> {
-    if (!isNavRender) {
-      elemNav.style = 'background-color: rgba(256,256,256,.7)';
-      elemNavLink.forEach(item => {
-        item.style = 'color: #000';
-        item.addEventListener('mouseenter', function() {
-          item.style = 'color: #24b4d7';
-        });
-        item.addEventListener('mouseleave', function() {
-          item.style = 'color: #000';
-        });
-      });
-      isNavRender = true;
-    }
-
-    let dY = this.scrollY;
-    let screenHeight = this.innerHeight;
-  
-    elemArr.forEach((item, index) => {
-      let elemItems = document.querySelectorAll(`.${item}`);
-      elemItems.forEach(element => {
-        if (element.classList.contains(`js-${elemArr[index]}`)) {
-          return;
-        }
-        if (dY > element.offsetTop - screenHeight / 2) {
-          element.classList.add(`js-${elemArr[index]}`);
-        }
-      });
-    });
+  const elemPlayBtn = document.querySelector('#PlayBtn');
+  document.addEventListener('scroll', () => {
+    renderNav();
+    scrollAni();
   });
-
   elemPlayBtn.addEventListener('click', showVideo);
   elemVideo.addEventListener('click', hideVideo);
   document.addEventListener('keyup', hideVideo);
+}
+
+function renderNav() {
+  if (!isNavRender) {
+    elemNav.style = 'background-color: rgba(256,256,256,.7)';
+    elemNavLink.forEach(item => {
+      item.style = 'color: #000';
+      item.addEventListener('mouseenter', () => {
+        item.style = 'color: #24b4d7';
+      });
+      item.addEventListener('mouseleave', () => {
+        item.style = 'color: #000';
+      });
+    });
+    isNavRender = true;
+  }
+}
+
+function scrollAni() {
+  let dY = this.scrollY;
+  let screenHeight = this.innerHeight;
+
+  arrAni.forEach(item => {
+    let elemItems = document.querySelectorAll(`.${item}`);
+    elemItems.forEach(element => {
+      if (element.classList.contains(`js-${item}`)) {
+        return;
+      }
+      if (dY > element.offsetTop - screenHeight / 2) {
+        element.classList.add(`js-${item}`);
+      }
+    });
+  });
 }
 
 function showVideo() {
