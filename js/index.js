3c49d453c8fa88c3ed761674291604d5e6b32aa7
Cynthia <76106817+cynthia101488@users.noreply.github.com>
2021-10-22 18:21:05 +0800

    20211022 update

--- js/index.js
+++ js/index.js
@@ -5,9 +5,6 @@ const elemNavBtn = document.querySelector('#NavBtn');
 const elemVideo = document.querySelector('#Video');
 const elemTop = document.querySelector('#Top');
 const aniArr = ['ani-fade-in-top', 'ani-fade-in-down'];
-let endTime = '';
-let personNum = 0;
-let totalNum = 100;
 let isEnd = false;
 let dateTimer = setInterval(renderTime, 1000);
 
@@ -15,7 +12,7 @@ getData();
 setEvent();
 
 function getData() {
-  const api = './data/activity.json';
+  const api = '../data/activity.json';
   fetch(api)
     .then(res => res.json())
     .then(data => {
@@ -24,15 +21,17 @@ function getData() {
 }
 
 function setTemplate(data) {
-  endTime = new Date(data['endTime']);
-  personNum = data['personNum'];
-  let arrList = data['list'];
-  renderTime();
-  renderProgress(arrList);
-  renderPerson();
+  let endTime = new Date(data.endTime);
+  let personNum = data.personNum;
+  const len = data.list.length;
+  let totalNum = data.list[len - 1].level;
+  let arrList = data.list;
+  renderTime(endTime, personNum);
+  renderProgress(arrList, personNum, totalNum);
+  renderPerson(personNum);
 }
 
-function renderTime() {
+function renderTime(endTime, personNum) {
   const elemTime = document.querySelector('#Time');
   const now = new Date();
   const diff = endTime - now;
@@ -68,53 +67,58 @@ function renderTime() {
   elemTime.innerHTML = str;
 }
 
-function renderProgress(arr) {
+// 整個結構需改善，3 個 <li>
+function renderProgress(arr, personNum, totalNum) {
   const elemProgress = document.querySelector('#Progress');
   const elemBar = document.querySelector('#Bar');
   let str = '';
+  let level = 0;
   arr.forEach(item => {
-    str += `<li class="signup__item" style="left: ${item.level/totalNum*100 + '%'}">
-              <div class="signup__top">
-                <span class="signup__text text--sm">達</span>
-                <span class="signup__text text--sm">${item.level}</span>
-                <span class="signup__text text--sm">人</span>
+    // str += `<li class="signup__item" style="left: ${item.level / totalNum * 100 + '%'}">
+    //           <div class="signup__top">
+    //             <span class="signup__text signup__text--sm">達</span>
+    //             <span class="signup__text signup__text--sm">${item.level}</span>
+    //             <span class="signup__text signup__text--sm">人</span>
+    //           </div>
+    //           <div class="signup__box ${personNum >= item.level ? 'signup__box--complete' : ''}"></div>
+    //           <div class="signup__bottom">
+    //             <span class="signup__text signup__text--sm">送 ${item.productName}</span>
+    //           </div>
+    //         </li>`;
+  
+    str += `<li class="signup__item" style="width: ${(item.level - level) / totalNum * 100 + '%'}">
+              <div class="signup__textbox signup__textbox--top">
+                <span class="signup__text signup__text--sm">達</span>
+                <span class="signup__text signup__text--sm">${item.level}</span>
+                <span class="signup__text signup__text--sm">人</span>
+              </div>
+              <div class="signup__line" style="width:  ${personNum >= item.level ? '100%' : (personNum - level) / (item.level - level) * 100 + '%'}"></div>
+              <div class="signup__textbox signup__textbox--bottom">
+                <span class="signup__text signup__text--sm">送 ${item.productName}</span>
               </div>
               <div class="signup__box ${personNum >= item.level ? 'signup__box--complete' : ''}"></div>
-              <div class="signup__bottom">
-                <span class="signup__text text--sm">送</span>
-                <span class="signup__text text--sm">${item.productName}</span>
-              </div>
             </li>`
+    level = item.level;
   });
 
-  elemProgress.innerHTML = `<li class="signup__item item-first">
-                              <div class="signup__box signup__box--complete"></div>
-                              <div class="signup__bottom">
-                                <span class="signup__text text--sm">預購開始</span>
-                              </div>
-                            </li>` + str;
-
-  elemBar.style = `width: ${personNum/totalNum*100 + '%'}`;
+  elemProgress.innerHTML = str;
+  // elemBar.style = `width: ${personNum / totalNum * 100 + '%'}`;
 }
 
-function renderPerson() {
+function renderPerson(personNum) {
   const elemPersonNum = document.querySelector('#PersonNum');
   let str = '';
   if (personNum < 100) {
     str = `<div class="signup__info" id="personNum">
             <span class="signup__text">已有</span>
-            <span class="signup__text text--lg">${personNum}</span>
+            <span class="signup__text signup__text--lg">${personNum}</span>
             <span class="signup__text">人報名</span>
           </div>
           <div class="signup__btn">
               <button class="btn" type="button">搶先報名 »</button>
           </div>`;
   } else {
-    if (!isEnd) {
-      str = `<span class="signup__full">已爆滿!</span>`
-    } else {
-      str = `<span class="signup__full">已額滿!</span>`
-    }
+    !isEnd ? str = `<span class="signup__full">已爆滿!</span>` : str = `<span class="signup__full">已額滿!</span>`
   }
   elemPersonNum.innerHTML = str;
 }
@@ -126,8 +130,7 @@ function setEvent() {
     scrollAni();
   });
 
-  elemNavBtn.addEventListener('click', showList);
-  document.addEventListener('click', hideList);
+  document.addEventListener('click', listStateChange);
   elemPlayBtn.addEventListener('click', showVideo);
   elemVideo.addEventListener('click', hideVideo);
   document.addEventListener('keyup', hideVideo);
@@ -136,32 +139,9 @@ function setEvent() {
 function renderNav() {
   let dY = this.scrollY;
   if (dY > 0) {
-    elemNav.style = 'background-color: rgba(256,256,256,.7)';
-    elemNavList.style.backgroundColor = 'rgba(256,256,256,0)';
-    elemNavLink.forEach(item => {
-      item.style.color = '#000';
-      item.addEventListener('mouseenter', () => {
-        item.style.color = '#24b4d7';
-      });
-      item.addEventListener('mouseleave', () => {
-        item.style.color = '#000';
-      });
-    });
+    elemNav.classList.add('js-nav-color');
   } else {
-    elemNav.style = 'background-color: rgba(256,256,256, 0)';
-    if (screen.width <= 480) {
-      elemNavList.style.backgroundColor = 'rgba(256,256,256,.7)';
-    }
-    elemNavLink.forEach(item => {
-      item.classList.remove('js-nav-top');
-      item.style.color = '#24b4d7';
-      item.addEventListener('mouseenter', () => {
-        item.style.color = '#000';
-      });
-      item.addEventListener('mouseleave', () => {
-        item.style.color = '#24b4d7';
-      });
-    });
+    elemNav.classList.remove('js-nav-color');
   }
 }
 
@@ -185,43 +165,56 @@ function scrollAni() {
   });
 }
 
+// function 扁平化
+// let count = 0;
+// let timer = '';
 function animateNum(item) {
   let count = 0;
-  const target = parseInt(item.innerText);
-  let timer = setInterval(renderNum, 10);
+  const target = parseInt(item.childNodes[1].dataset.num);
+  // timer = setInterval(renderNum, 10, item, target, timer);
+  let timer = setInterval(renderNum, 5);
   function renderNum() {
     count = count + 1;
-    item.childNodes[1].innerText = count;
+    item.childNodes[1].textContent = count;
     if (count === target) {
       clearInterval(timer);
     }
   }
 }
 
-function showList(e) {
-  e.stopPropagation();
-  elemNavList.classList.toggle('js-nav__ls');
-}
+// function renderNum(item, target, timer) {
+//   count = count + 1;
+//   item.childNodes[1].textContent = count;
+//   if (count === target) {
+//     clearInterval(timer)
+//   }
+// }
 
-function hideList(e) {
+function listStateChange(e) {
+  e.stopPropagation();
   const self = e.target;
-  if (self.className !== 'nav__item' && self.className !== 'link') {
-    elemNavList.classList.remove('js-nav__ls');
+  if (self.nodeName === 'I') {
+    elemNavList.classList.toggle('js-nav__ls');
+  } else {
+    if (self.nodeName !== 'LI') {
+      elemNavList.classList.remove('js-nav__ls');
+    }
   }
 }
 
 function showVideo() {
-  elemVideo.style = 'display: block';
-  document.body.style = 'overflow: hidden';
-  elemTop.style = 'display: none';
-  elemVideo.querySelector('iframe').src = 'https://www.youtube.com/embed/syFyL9tONRA';
+  document.body.classList.add('js-lock');
+  elemVideo.classList.add('js-show');
+  elemTop.classList.remove('js-show');
+  const url = elemVideo.querySelector('iframe').dataset.src;
+  elemVideo.querySelector('iframe').src = url;
 }
 
 function hideVideo(e) {
   if (e.type === 'click' || e.keyCode === 27) {
-    elemVideo.style = 'display: none';
-    document.body.style = 'overflow: auto';
-    elemTop.style = 'display: block';
+    elemVideo.classList.remove('js-show');
+    document.body.classList.remove('js-lock');
+    elemTop.classList.add('js-show');
     elemVideo.querySelector('iframe').src = '';
   }
 }
