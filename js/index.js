d2ee2c7f66507d14c13942877561712dbe196868
Cynthia <76106817+cynthia101488@users.noreply.github.com>
2021-10-25 11:27:51 +0800

    20211025 update

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ getData();
 setEvent();
 
 function getData() {
-  const api = '../data/activity.json';
+  const api = './data/activity.json';
   fetch(api)
     .then(res => res.json())
     .then(data => {
